30c2b3fbe766dca9cbeba3b1f24030f054e05979
Mduduzi Nzimande <admin@senreques.co.za>
2023-06-19 02:44:48 -0700

    Updates

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - puts char
+ * Description: prints a string
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
